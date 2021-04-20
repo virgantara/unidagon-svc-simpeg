e637e09022dc9ff1d9a828b4d449511fc9ec8771
virgantara <oddy@unida.gontor.ac.id>
2021-04-20 11:50:09 +0700

    change to non persistent

--- db.js
+++ db.js
@@ -1,23 +1,26 @@
 const dbConfig = require('./config/db.json');
 var mysql = require('mysql')
-var pool = mysql.createPool(dbConfig)
+var db = mysql.createConnection(dbConfig)
+// var pool = mysql.createPool(dbConfig)
 
-pool.getConnection((err, connection) => {
-    if (err) {
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            console.error('Database connection was closed.')
-        }
-        if (err.code === 'ER_CON_COUNT_ERROR') {
-            console.error('Database has too many connections.')
-        }
-        if (err.code === 'ECONNREFUSED') {
-            console.error('Database connection was refused.')
-        }
-    }
 
-    if (connection) connection.release()
 
-    return
-})
+// pool.getConnection((err, connection) => {
+//     if (err) {
+//         if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+//             console.error('Database connection was closed.')
+//         }
+//         if (err.code === 'ER_CON_COUNT_ERROR') {
+//             console.error('Database has too many connections.')
+//         }
+//         if (err.code === 'ECONNREFUSED') {
+//             console.error('Database connection was refused.')
+//         }
+//     }
 
-module.exports = pool
+//     if (connection) connection.release()
+
+//     return
+// })
+// module.exports = pool
+module.exports = db
