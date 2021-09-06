ac0e890837eed63dcc8f03454f8835261954f62b
virgantara <oddy@unida.gontor.ac.id>
2021-09-06 23:09:50 +0700

    + kode unik

--- app/model/appModel.js
+++ app/model/appModel.js
@@ -1089,7 +1089,7 @@ function getProfilDosen(data,callback){
         txt += " d.tanggal_lahir, p.nama as pangkat, p.golongan, j.nama as jabfung, "
         txt += " d.jenjang_kode, pr.nama as nama_prodi, u.status, bi.nama as bidang_ilmu, "
         txt += " bii.nama as bidang_ilmu_induk, bk.nama as kepakaran, bkp.nama as parent_kepakaran, "
-        txt += " d.permalink, d.expertise, u.uuid, (SELECT GROUP_CONCAT(item_name) FROM auth_assignment WHERE u.id = user_id) as roles from data_diri d "
+        txt += " d.permalink, d.expertise, u.sister_id, u.uuid, (SELECT GROUP_CONCAT(item_name) FROM auth_assignment WHERE u.id = user_id) as roles from data_diri d "
         txt += " JOIN m_pangkat p on p.id = d.pangkat"
         txt += " JOIN m_jabatan_akademik j on j.id = d.jabatan_fungsional"
         txt += " JOIN user u ON u.NIY = d.NIY "
@@ -1108,6 +1108,11 @@ function getProfilDosen(data,callback){
         if(data.NIY){
             txt += " AND d.NIY = ? "
             params.push(data.NIY)
+        }
+
+        if(data.kode_unik){
+            txt += " AND d.kode_unik = ? "
+            params.push(data.kode_unik)
         }
 
         if(data.telegram_username){
